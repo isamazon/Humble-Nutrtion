8d9a981045e9c551f0e6793f9c077e3f246ab213
isamazon <37008425+isamazon@users.noreply.github.com>
2021-02-08 15:30:24 -0600

    Added the absolute basics for the menu modal

---
+++ source/menumodal.js
@@ -0,0 +1,9 @@
+'use strict'
+
+const openModalbtns = {
+    unsweet: document.getElementById('unsweetTea')
+}
+
+openModalbtns.unsweet.addEventListener('click', () => {
+    console.log('click')
+})
