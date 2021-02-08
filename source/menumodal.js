dc7c4273d664ca1f3ac0b05560c9c2b91beb1976
isamazon <37008425+isamazon@users.noreply.github.com>
2021-02-08 17:45:15 -0600

    Added open/close function to the motal
    Added a open and close function to the smoothie menu modal

--- source/menumodal.js
+++ source/menumodal.js
@@ -1,9 +1,20 @@
 'use strict'
 
 const openModalbtns = {
-    unsweet: document.getElementById('unsweetTea')
+    unsweet: document.getElementById('unsweetTea'),
+    sweet: document.getElementById('sweetTea'),
+    special: document.getElementById('specialDrink'),
+    addon: document.getElementById('addonDrinks'),
+    smoothies: document.getElementById('smoothies-drink'),
 }
+const modal = document.querySelector('.modal-1-bg')
+// Close modal buttons
+const closeModal = document.querySelector('.close-modal-button');
 
-openModalbtns.unsweet.addEventListener('click', () => {
-    console.log('click')
+closeModal.addEventListener('click', () => {
+    modal.style.display = 'none'
+})
+// open Smootie modal
+openModalbtns.smoothies.addEventListener('click', () => {
+    modal.style.display = 'flex' 
 })
