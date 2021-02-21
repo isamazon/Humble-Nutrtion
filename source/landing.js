c7e7ed5382e95af37416e28f76fc8d45537a674a
isamazon <37008425+isamazon@users.noreply.github.com>
2021-02-20 23:23:24 -0700

    Version where the slide is 3 per view under 500px
    Setting this commit for the version where the slider has 3 per view and works under 500px im going for a single per view after this change

--- source/landing.js
+++ source/landing.js
@@ -1,10 +1,14 @@
 'use strict';
 const pages = {
     aboutMe: document.querySelector('.second-container'),
+    shakeSelector: document.querySelector('.landing-page-shake-container'),
+
 }
 // Creating a on click function inside the drink carousel where on the click the color to the circle, and the h1 changes to the corresponding drink color
 // variable for the tea pic
 const teaPic = document.querySelector('.tea-pic');
+// Variable for the fa arrow btn 
+const arrowBtn1 = document.querySelector('.fa-angle-double-right');
 // Variable for the circle color
 const circleColor = document.querySelector('.circle');
 // Variable for the header color
@@ -13,12 +17,20 @@ const headerColor = document.querySelector('.nutrtion-color');
 const paragraph = document.querySelector('.p-color');
 // Variable for the learn more button
 const lrnBtn = document.querySelector('.learnBtn');
-// A variable containing all of the above to try and make the code footprint less
-const selectColor = document.getElementById('color-selector');
+// Variable for the header span
+const headerSpan = document.querySelector('.header-span');
 
+
+// A variable containing all of the above to try and make the code footprint less
+// const selectColor = document.querySelectorAll('.color-selector');
+// console.log(selectColor)
 lrnBtn.addEventListener('click', () => {
-    console.log('click');
+    
     pages.aboutMe.scrollIntoView({behavior: "smooth", block: "start", inline: "nearest"});
+})
+arrowBtn1.addEventListener('click', () => {
+    console.log('clickkkkkk');
+    pages.shakeSelector.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
 })
 
 
@@ -61,6 +73,7 @@ function drinkSlider(image) {
         lrnBtn.style.backgroundColor = '#048163';
         headerColor.style.color = '#048163';
         paragraph.style.color ='#048163';
+        headerSpan.style.color = '#048163';
         animation2;
         paragraph.textContent = 'Where we have delicious energizing drinks!';
         animation1;
@@ -69,15 +82,17 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#f6cd58';
         lrnBtn.style.backgroundColor = '#f6cd58';
         headerColor.style.color = '#f6cd58';
+        headerSpan.style.color = '#f6cd58';
         paragraph.style.color ='#f6cd58'
         animation2;
         paragraph.textContent = 'Where we have delicious energizing drinks!'
         animation1;
     } else if (image === '/css/pictures/tea3S.png') {
-        circleColor.style.backgroundColor = '#0062be';
-        lrnBtn.style.backgroundColor = '#0062be';
-        headerColor.style.color = '#0062be';
-        paragraph.style.color ='#0062be'
+        circleColor.style.backgroundColor = '#00bebe';
+        lrnBtn.style.backgroundColor = '#00bebe';
+        headerColor.style.color = '#00bebe';
+        headerSpan.style.color = '#00bebe';
+        paragraph.style.color ='#00bebe'
         animation2
         paragraph.textContent = 'Where we have delicious energizing drinks!'
         animation1;
@@ -86,6 +101,7 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#2f231b';
         lrnBtn.style.backgroundColor = '#2f231b';
         headerColor.style.color = '#2f231b';
+        headerSpan.style.color = '#2f231b';
         paragraph.style.color ='#2f231b';
         animation2;
         paragraph.textContent = 'Where we have delicious protein packed shakes!';
@@ -94,6 +110,7 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#eeb269';
         lrnBtn.style.backgroundColor = '#eeb269';
         headerColor.style.color = '#eeb269';
+        headerSpan.style.color = '#eeb269';
         paragraph.style.color ='#eeb269'
         animation2;
         paragraph.textContent = 'Where we have delicious protein packed shakes!';
@@ -102,6 +119,7 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#c2b7a1';
         lrnBtn.style.backgroundColor = '#c2b7a1';
         headerColor.style.color = '#c2b7a1';
+        headerSpan.style.color = '#c2b7a1';
         paragraph.style.color ='#c2b7a1'
         animation2;
         paragraph.textContent = 'Where we have delicious protein packed shakes!';
@@ -113,6 +131,7 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#f1342f';
         lrnBtn.style.backgroundColor = '#f1342f';
         headerColor.style.color = '#f1342f';
+        headerSpan.style.color = '#f1342f';
         paragraph.style.color ='#f1342f'
         animation2;
         paragraph.textContent = 'Where we have delicious nutritious protien packed donuts!';
@@ -122,6 +141,7 @@ function drinkSlider(image) {
         circleColor.style.backgroundColor = '#251b1c';
         lrnBtn.style.backgroundColor = '#251b1c';
         headerColor.style.color = '#251b1c';
+        headerSpan.style.color = '#251b1c';
         paragraph.style.color ='#251b1c'
         animation2;
         paragraph.textContent = 'Where we have delicious nutritious protien packed donuts!';
